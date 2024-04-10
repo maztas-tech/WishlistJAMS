99cceba888ee01f6fc0b19ec9580435ef8e9a558
maztas-tech <144379432+maztas-tech@users.noreply.github.com>
2024-04-10 19:40:32 +0200

    Added dependency injection and requestmapping bean in Controller class.

--- src/main/java/wishlist/controller/WishlistController.java
+++ src/main/java/wishlist/controller/WishlistController.java
@@ -1,7 +1,19 @@
 package wishlist.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import wishlist.model.Wishlist;
+import wishlist.service.WishlistService;
 
 @Controller
+@RequestMapping("wish_list")
 public class WishlistController {
+    private WishlistService wishlistService;
+    public WishlistController(WishlistService wishlistService) {
+        this.wishlistService = wishlistService;
+    }
+
+
 }
--- src/main/java/wishlist/service/WishlistService.java
+++ src/main/java/wishlist/service/WishlistService.java
@@ -1,7 +1,14 @@
 package wishlist.service;
 
 import org.springframework.stereotype.Service;
+import wishlist.repository.WishlistRepository;
 
 @Service
 public class WishlistService {
+    private WishlistRepository wishlistRepository;
+    public WishlistService(WishlistRepository wishlistRepository) {
+        this.wishlistRepository = wishlistRepository;
+    }
+
+
 }
