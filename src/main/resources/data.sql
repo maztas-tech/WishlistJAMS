71fbbb7345a64db9bb5770687b7cac43c573bb1a
jawadaf <jafa0001@stud.kea.dk>
2024-04-11 17:26:23 +0200

    I have defined our table design and created connection to our h2 database

---
+++ src/main/resources/data.sql
@@ -0,0 +1,21 @@
+CREATE SCHEMA if not exists wishlist_db;
+use wishlist_db;
+
+CREATE TABLE if not exists wish(
+    wishID INT auto_increment,
+    wishName VARCHAR(50) NOT NULL,
+    wishDescription VARCHAR(100),
+    wishPrice INT NOT NULL,
+    primary key(wishID)
+);
+
+CREATE TABLE if not exists wishlist(
+    wishlistID INT auto_increment,
+    isPrivate BOOLEAN,
+    wishlistName VARCHAR(50) NOT NULL,
+    wishlistDescription VARCHAR(100),
+    wishlistPrice INT NOT NULL,
+    wishID INT,
+    primary key(wishlistID),
+    foreign key(wishID) references wish(wishID)
+);
